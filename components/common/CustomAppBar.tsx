c0653dcfb668ac9be6070c06387f7f186c2edff2
sunghoon <shun1423@naver.com>
2025-03-20 01:36:49 +0900

    모바일 로고

--- components/common/CustomAppBar.tsx
+++ components/common/CustomAppBar.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { useRouter } from "next/router";
+import Image from "next/image";
 import {
   Box,
   Container,
@@ -19,6 +20,7 @@ import MenuToggler from "components/common/MenuToggler";
 import ConstantsContext from "context/constantsContext";
 import useOnClickOutside from "hooks/useOnClickOutside";
 import ComponentsContext from "context/componentsContext";
+import NameLogo from "components/common/NameLogo";
 
 const CustomAppBarRoot = styled("div")(({ theme }) => ({
   // 필요시 AppBar 스타일을 적용하되,
@@ -27,9 +29,32 @@ const CustomAppBarRoot = styled("div")(({ theme }) => ({
 }));
 
 const CustomToolbar = styled(Toolbar)(({ theme }) => ({
+  display: "flex",
+  justifyContent: "space-between", // 로고와 메뉴 토글러 사이 공간 확보
+  padding: "8px 0", // 상하 패딩 추가
   [theme.breakpoints.down("md")]: {
-    padding: 0,
+    padding: "8px 0",
   },
+}));
+
+// 로고 컨테이너 스타일
+const LogoContainer = styled(Box)(() => ({
+  display: "flex",
+  alignItems: "center",
+  cursor: "pointer",
+}));
+
+// 로고 이미지 컨테이너
+const CompanyLogoWrapper = styled(Box)(() => ({
+  position: "relative",
+  width: "40px", // 모바일에서는 조금 작게
+  height: "40px",
+  marginRight: "8px",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  borderRadius: "8px", // 살짝 둥근 모서리
+  overflow: "hidden",
 }));
 
 const CustomAppBar: React.FC = () => {
@@ -51,18 +76,44 @@ const CustomAppBar: React.FC = () => {
     router.push(href);
   };
 
+  const handleLogoClick = () => {
+    router.push("/");
+  };
+
   const theme = useTheme<Theme>();
   const isLargeScreen = useMediaQuery(theme.breakpoints.up("sm"));
 
   return (
     <CustomAppBarRoot ref={appBarRef}>
-      {/* 불필요한 height: 0 제거 */}
-      <Container maxWidth={containerMaxWidth} sx={{ padding: 0, margin: 0 }}>
+      <Container
+        maxWidth={containerMaxWidth}
+        sx={{ padding: "0 16px", margin: 0 }}
+      >
         <CustomToolbar>
-          <Box sx={{ marginLeft: "auto" }}>
-            {/* 모바일에서만 메뉴 목록 */}
+          {/* 로고 영역 (항상 표시) */}
+          <LogoContainer onClick={handleLogoClick}>
+            <CompanyLogoWrapper>
+              <Image
+                src="/회사로고.jpeg"
+                alt="Company Logo"
+                width={40}
+                height={40}
+                style={{
+                  objectFit: "cover",
+                }}
+              />
+            </CompanyLogoWrapper>
+            <NameLogo
+              name="GothiC"
+              style={{
+                height: "50px",
+                paddingTop: "3px", // 살짝 아래로 조정
+              }}
+            />
+          </LogoContainer>
 
-            {/* 햄버거 아이콘 */}
+          {/* 햄버거 아이콘 (모바일에서만 표시) */}
+          <Box>
             <MenuToggler
               color="info"
               onClick={() => setDropdownState(!dropdownState)}
